8c3451045a11d25b2275180abe6c6728bd8a20bf
Brian Walsh <Brian.Walsh+1-at-432593804115>
2019-06-10 09:41:15 -0700

    finished my setup for my 'deep' integration test

---
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -0,0 +1,42 @@
+import { HeroesComponent } from "../heroes/heroes.component";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { NO_ERRORS_SCHEMA, Component, Input } from "@angular/core";
+import { HeroService } from "../hero.service";
+import { of } from "rxjs/internal/observable/of"
+import { Hero } from "../hero";
+import { By } from "@angular/platform-browser";
+import { HeroComponent } from "../hero/hero.component";
+
+describe('HeroesComponent (deep tests)', () => {
+    let fixture: ComponentFixture<HeroesComponent>;
+    let mockHeroService;
+    let HEROES;
+
+    beforeEach(() => {
+        mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+        HEROES = [
+            {id: 1, name: 'SpiderDude', strength: 8},
+            {id: 2, name: 'Wonderful Woman', strength: 24},
+            {id: 3, name: 'SuperDude', strength: 55}
+        ];
+
+        TestBed.configureTestingModule({
+            declarations: [
+                HeroesComponent,
+                HeroComponent
+            ],
+            providers: [
+                {provide: HeroService, useValue: mockHeroService}
+            ],
+            schemas: [NO_ERRORS_SCHEMA]
+        })
+        fixture = TestBed.createComponent(HeroesComponent);
+        mockHeroService.getHeroes.and.returnValue(of(HEROES));
+
+        fixture.detectChanges();
+    });
+
+    it('should be true', () => {
+        expect(true).toBe(true);
+    });
+});
